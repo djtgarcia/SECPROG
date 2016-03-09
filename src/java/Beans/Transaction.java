2471eee576dd28d4da9bd7ef4a58417ca3d5dd92
djtgarcia <djtgarcia720@gmail.com>
2016-03-09 19:17:26 +0800

    Added transaction class

---
+++ src/java/Beans/Transaction.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Beans;
+
+import java.sql.Timestamp;
+
+public class Transaction {
+
+    private int transactionID;
+    private Timestamp transactionDate;
+    private int purchaseID;
+    private int quantity;
+    private float price;
+    private int productID;
+    private String username;
+    private String title;
+    private float totalSales;
+    private String type;
+    
+    public Transaction() {
+        
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public Timestamp getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Timestamp transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public int getPurchaseID() {
+        return purchaseID;
+    }
+
+    public void setPurchaseID(int purchaseID) {
+        this.purchaseID = purchaseID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(float totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+}
